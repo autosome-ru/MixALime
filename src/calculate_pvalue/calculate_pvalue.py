4b7189ca054cbafa627a55d7100ca592ab2c52f3
Sergey Abramov <aswq22013@gmail.com>
2022-01-17 13:05:24 +0300

    debug

--- src/calculate_pvalue/calculate_pvalue.py
+++ src/calculate_pvalue/calculate_pvalue.py
@@ -164,8 +164,7 @@ def get_neg_bin_params(fit_param, main_allele, BAD, err_id):
 def get_pmf_for_dist(params, k, m, BAD, model):
     p = get_p(BAD)
     if model == 'BetaNB':
-        print(params['logpdf']['modes'])
-        logpdfs = map(lambda x: x[k], params['logpdf']['modes'])  # Use m as key
+        logpdfs = map(lambda x: x[m][k], params['logpdf']['modes'])  # Use m as key
         print(logpdfs)
         return 0, 0
     else:
